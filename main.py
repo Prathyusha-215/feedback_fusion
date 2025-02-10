967cb45973f749c3a7fa52761e69e547d8248117
prathyusha <prathyushavanama215@gmail.com>
2025-02-10 16:26:43 +0530

    Added requirements.txt

--- main.py
+++ main.py
@@ -21,6 +21,16 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+
 # Download NLTK VADER lexicon if not already downloaded
 nltk.download('vader_lexicon')
 sid = SentimentIntensityAnalyzer()
